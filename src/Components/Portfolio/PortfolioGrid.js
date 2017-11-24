d8b49e9d052cf928d7572a784c090ebc1b0205f2
Gildon Kirk <gildonkirk@gmail.com>
2017-11-24 11:44:57 -0500

    side scroll replaces carousel

--- src/Components/Portfolio/PortfolioGrid.js
+++ src/Components/Portfolio/PortfolioGrid.js
@@ -3,48 +3,16 @@ import React, { Component } from 'react';
 class PortfolioGrid extends Component {
   render() {
     return (
-      <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-        <ol className="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img className="d-block w-100" src={require("../../assets/music_wide.jpg")} alt="First slide" />
-          </div>
-          <div className="carousel-item">
-            <img className="d-block w-100" src={require("../../assets/got_wide.jpg")} alt="Second slide" />
-          </div>
-          <div className="carousel-item">
-            <img className="d-block w-100" src={require("../../assets/gif_wide.jpg")} alt="Third slide" />
-          </div>
+      <div className="outer">
+        <div className="float-wrap">
+          <img className='img-fluid left-floater' src={require("../../assets/music.jpg")} alt=""/>
+          <img className='img-fluid left-floater' src={require("../../assets/got.jpg")} alt=""/>
+          <img className='img-fluid left-floater' src={require("../../assets/gif.jpg")} alt=""/>
+          <img className='img-fluid left-floater' src={require("../../assets/starwars.jpg")} alt=""/>
         </div>
-        <a className="carousel-control-prev carouselControl" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a className="carousel-control-next carouselControl" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="sr-only">Next</span>
-        </a>
       </div>
     )
   };
 }
 
 export default PortfolioGrid;
-// <div className="row gridRow">
-//   <div className='col-4 gridItem'>
-//     <img className='img-fluid' src={require("../../assets/music.jpg")} alt=""/>
-//   </div>
-//   <div className='col-4 gridItem'>
-//     <img className='img-fluid' src={require("../../assets/got.jpg")} alt=""/>
-//   </div>
-//   <div className='col-4 gridItem'>
-//     <img className='img-fluid' src={require("../../assets/gif.jpg")} alt=""/>
-//   </div>
-//   <div className='col-4 gridItem'>
-//     <img className='img-fluid' src={require("../../assets/starwars.jpg")} alt=""/>
-//   </div>
-// </div>
