180d6833ab57b1121a235c8f0cc01a8ef8a6f585
Gildon Kirk <gildonkirk@gmail.com>
2017-11-24 12:14:36 -0500

    img hover added, width of portfolio images container fixed

--- src/Components/Portfolio/PortfolioGrid.js
+++ src/Components/Portfolio/PortfolioGrid.js
@@ -5,10 +5,10 @@ class PortfolioGrid extends Component {
     return (
       <div className="outer">
         <div className="float-wrap">
-          <img className='img-fluid left-floater' src={require("../../assets/music.jpg")} alt=""/>
-          <img className='img-fluid left-floater' src={require("../../assets/got.jpg")} alt=""/>
-          <img className='img-fluid left-floater' src={require("../../assets/gif.jpg")} alt=""/>
-          <img className='img-fluid left-floater' src={require("../../assets/starwars.jpg")} alt=""/>
+          <img className='img-fluid img-hover left-floater' src={require("../../assets/music.jpg")} alt=""/>
+          <img className='img-fluid img-hover left-floater' src={require("../../assets/got.jpg")} alt=""/>
+          <img className='img-fluid img-hover left-floater' src={require("../../assets/gif.jpg")} alt=""/>
+          <img className='img-fluid img-hover left-floater' src={require("../../assets/starwars.jpg")} alt=""/>
         </div>
       </div>
     )
