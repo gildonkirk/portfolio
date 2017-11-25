97647fe86270965df64fc48946c560e62f0ad000
Gildon Kirk <gildonkirk@gmail.com>
2017-11-25 14:44:21 -0500

    opaque background color added to img on hover

--- src/Components/Portfolio/PortfolioGrid.js
+++ src/Components/Portfolio/PortfolioGrid.js
@@ -5,10 +5,34 @@ class PortfolioGrid extends Component {
     return (
       <div className="outer" id="scroll">
         <div className="float-wrap">
-          <a href="https://github.com/kyscott/Music-To-Date" target="_blank"><img className='img-fluid img-hover left-floater' src={require("../../assets/music.jpg")} alt=""/></a>
-          <a href="https://gildonkirk.github.io/TriviaGame/" target="_blank"><img className='img-fluid img-hover left-floater' src={require("../../assets/got.jpg")} alt=""/></a>
-          <a href="https://gildonkirk.github.io/gif-er-izer/" target="_blank"><img className='img-fluid img-hover left-floater' src={require("../../assets/gif.jpg")} alt=""/></a>
-          <a href="https://gildonkirk.github.io/star-wars-tbs/" target="_blank"><img className='img-fluid img-hover left-floater' src={require("../../assets/starwars.jpg")} alt=""/></a>
+          <div className="img-link">
+            <a href="https://github.com/kyscott/Music-To-Date" target="_blank">
+              <img className='img-fluid img-hover left-floater' src={require("../../assets/music.jpg")} alt=""/>
+            </a>
+            <div className="opaque">
+            </div>
+          </div>
+          <div className="img-link">
+            <a href="https://gildonkirk.github.io/TriviaGame/" target="_blank">
+              <img className='img-fluid img-hover left-floater' src={require("../../assets/got.jpg")} alt=""/>
+            </a>
+            <div className="opaque">
+            </div>
+          </div>
+          <div className="img-link">
+            <a href="https://gildonkirk.github.io/gif-er-izer/" target="_blank">
+              <img className='img-fluid img-hover left-floater' src={require("../../assets/gif.jpg")} alt=""/>
+            </a>
+            <div className="opaque">
+            </div>
+          </div>
+          <div className="img-link">
+            <a href="https://gildonkirk.github.io/star-wars-tbs/" target="_blank">
+              <img className='img-fluid img-hover left-floater' src={require("../../assets/starwars.jpg")} alt=""/>
+            </a>
+            <div className="opaque">
+            </div>
+          </div>
         </div>
       </div>
     )
