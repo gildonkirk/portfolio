776bf8c76b0673d93b28821844716c9d211e44a7
Gildon Kirk <gildonkirk@gmail.com>
2017-11-25 13:30:04 -0500

    links added to portfolio images

--- src/Components/Portfolio/PortfolioGrid.js
+++ src/Components/Portfolio/PortfolioGrid.js
@@ -5,10 +5,10 @@ class PortfolioGrid extends Component {
     return (
       <div className="outer" id="scroll">
         <div className="float-wrap">
-          <img className='img-fluid img-hover left-floater' src={require("../../assets/music.jpg")} alt=""/>
-          <img className='img-fluid img-hover left-floater' src={require("../../assets/got.jpg")} alt=""/>
-          <img className='img-fluid img-hover left-floater' src={require("../../assets/gif.jpg")} alt=""/>
-          <img className='img-fluid img-hover left-floater' src={require("../../assets/starwars.jpg")} alt=""/>
+          <a href="https://github.com/kyscott/Music-To-Date" target="_blank"><img className='img-fluid img-hover left-floater' src={require("../../assets/music.jpg")} alt=""/></a>
+          <a href="https://gildonkirk.github.io/TriviaGame/" target="_blank"><img className='img-fluid img-hover left-floater' src={require("../../assets/got.jpg")} alt=""/></a>
+          <a href="https://gildonkirk.github.io/gif-er-izer/" target="_blank"><img className='img-fluid img-hover left-floater' src={require("../../assets/gif.jpg")} alt=""/></a>
+          <a href="https://gildonkirk.github.io/star-wars-tbs/" target="_blank"><img className='img-fluid img-hover left-floater' src={require("../../assets/starwars.jpg")} alt=""/></a>
         </div>
       </div>
     )
