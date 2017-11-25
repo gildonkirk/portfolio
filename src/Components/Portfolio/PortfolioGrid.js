dafa5ec269d64244f37ad185d267f9d3844544f5
Gildon Kirk <gildonkirk@gmail.com>
2017-11-25 12:05:31 -0500

    scrollbar css started

--- src/Components/Portfolio/PortfolioGrid.js
+++ src/Components/Portfolio/PortfolioGrid.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class PortfolioGrid extends Component {
   render() {
     return (
-      <div className="outer">
+      <div className="outer" id="scroll">
         <div className="float-wrap">
           <img className='img-fluid img-hover left-floater' src={require("../../assets/music.jpg")} alt=""/>
           <img className='img-fluid img-hover left-floater' src={require("../../assets/got.jpg")} alt=""/>
