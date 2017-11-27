0bc41e6c23ff5a366d23052f5710d2126a4c9c1c
Gildon Kirk <gildonkirk@gmail.com>
2017-11-27 12:14:59 -0500

    links to websites work again

--- src/Components/Portfolio/PortfolioGrid.js
+++ src/Components/Portfolio/PortfolioGrid.js
@@ -6,44 +6,51 @@ class PortfolioGrid extends Component {
       <div className="outer" id="scroll">
         <div className="float-wrap">
           <div className="img-link">
+            <img className='img-fluid img-hover left-floater' src={require("../../assets/music.jpg")} alt=""/>
             <a href="https://github.com/kyscott/Music-To-Date" target="_blank">
-              <img className='img-fluid img-hover left-floater' src={require("../../assets/music.jpg")} alt=""/>
+              <div className="opaque"></div>
             </a>
-            <div className="opaque">
-            </div>
-            <div className="opaque-label">
-              Music to Date
-            </div>
+            <a href="https://github.com/kyscott/Music-To-Date" target="_blank">
+              <div className="opaque-label">
+                Music to Date
+              </div>
+            </a>
           </div>
           <div className="img-link">
+            <img className='img-fluid img-hover left-floater' src={require("../../assets/got.jpg")} alt=""/>
             <a href="https://gildonkirk.github.io/TriviaGame/" target="_blank">
-              <img className='img-fluid img-hover left-floater' src={require("../../assets/got.jpg")} alt=""/>
+              <div className="opaque">
+              </div>
             </a>
-            <div className="opaque">
-            </div>
-            <div className="opaque-label">
-              GOT Trivia
-            </div>
+            <a href="https://gildonkirk.github.io/TriviaGame/" target="_blank">
+              <div className="opaque-label">
+                GOT Trivia
+              </div>
+            </a>
           </div>
           <div className="img-link">
+            <img className='img-fluid img-hover left-floater' src={require("../../assets/gif.jpg")} alt=""/>
             <a href="https://gildonkirk.github.io/gif-er-izer/" target="_blank">
-              <img className='img-fluid img-hover left-floater' src={require("../../assets/gif.jpg")} alt=""/>
+              <div className="opaque">
+              </div>
             </a>
-            <div className="opaque">
-            </div>
-            <div className="opaque-label">
-              Gif-er-izer
-            </div>
+            <a href="https://gildonkirk.github.io/gif-er-izer/" target="_blank">
+              <div className="opaque-label">
+                Gif-er-izer
+              </div>
+            </a>
           </div>
           <div className="img-link">
+            <img className='img-fluid img-hover left-floater' src={require("../../assets/starwars.jpg")} alt=""/>
             <a href="https://gildonkirk.github.io/star-wars-tbs/" target="_blank">
-              <img className='img-fluid img-hover left-floater' src={require("../../assets/starwars.jpg")} alt=""/>
+              <div className="opaque">
+              </div>
             </a>
-            <div className="opaque">
-            </div>
-            <div className="opaque-label">
-              Star Wars Game
-            </div>
+            <a href="https://gildonkirk.github.io/star-wars-tbs/" target="_blank">
+              <div className="opaque-label">
+                Star Wars Game
+              </div>
+            </a>
           </div>
         </div>
       </div>
