682edd189a5e608d2b4db55ec0550298e7e40c46
Gildon Kirk <gildonkirk@gmail.com>
2017-11-28 11:52:23 -0500

    margin-left of first img removed, added rel to links for security reasons seen in console

--- src/Components/Portfolio/PortfolioGrid.js
+++ src/Components/Portfolio/PortfolioGrid.js
@@ -5,12 +5,12 @@ class PortfolioGrid extends Component {
     return (
       <div className="outer" id="scroll">
         <div className="float-wrap">
-          <div className="img-link">
+          <div className="img-link" id="img-one">
             <img className='img-fluid img-hover left-floater' src={require("../../assets/music.jpg")} alt=""/>
-            <a href="https://github.com/kyscott/Music-To-Date" target="_blank">
+            <a href="https://github.com/kyscott/Music-To-Date" target="_blank" rel="noopener noreferrer">
               <div className="opaque"></div>
             </a>
-            <a href="https://github.com/kyscott/Music-To-Date" target="_blank">
+            <a href="https://github.com/kyscott/Music-To-Date" target="_blank" rel="noopener noreferrer">
               <div className="opaque-label">
                 Music to Date
               </div>
@@ -18,11 +18,11 @@ class PortfolioGrid extends Component {
           </div>
           <div className="img-link">
             <img className='img-fluid img-hover left-floater' src={require("../../assets/got.jpg")} alt=""/>
-            <a href="https://gildonkirk.github.io/TriviaGame/" target="_blank">
+            <a href="https://gildonkirk.github.io/TriviaGame/" target="_blank" rel="noopener noreferrer">
               <div className="opaque">
               </div>
             </a>
-            <a href="https://gildonkirk.github.io/TriviaGame/" target="_blank">
+            <a href="https://gildonkirk.github.io/TriviaGame/" target="_blank" rel="noopener noreferrer">
               <div className="opaque-label">
                 GOT Trivia
               </div>
@@ -30,11 +30,11 @@ class PortfolioGrid extends Component {
           </div>
           <div className="img-link">
             <img className='img-fluid img-hover left-floater' src={require("../../assets/gif.jpg")} alt=""/>
-            <a href="https://gildonkirk.github.io/gif-er-izer/" target="_blank">
+            <a href="https://gildonkirk.github.io/gif-er-izer/" target="_blank" rel="noopener noreferrer">
               <div className="opaque">
               </div>
             </a>
-            <a href="https://gildonkirk.github.io/gif-er-izer/" target="_blank">
+            <a href="https://gildonkirk.github.io/gif-er-izer/" target="_blank" rel="noopener noreferrer">
               <div className="opaque-label">
                 Gif-er-izer
               </div>
@@ -42,11 +42,11 @@ class PortfolioGrid extends Component {
           </div>
           <div className="img-link">
             <img className='img-fluid img-hover left-floater' src={require("../../assets/starwars.jpg")} alt=""/>
-            <a href="https://gildonkirk.github.io/star-wars-tbs/" target="_blank">
+            <a href="https://gildonkirk.github.io/star-wars-tbs/" target="_blank" rel="noopener noreferrer">
               <div className="opaque">
               </div>
             </a>
-            <a href="https://gildonkirk.github.io/star-wars-tbs/" target="_blank">
+            <a href="https://gildonkirk.github.io/star-wars-tbs/" target="_blank" rel="noopener noreferrer">
               <div className="opaque-label">
                 Star Wars Game
               </div>
