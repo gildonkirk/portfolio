aff74ba18bffcf3ade0d90e2b92b3a32a6104c2d
Gildon Kirk <gildonkirk@gmail.com>
2018-01-09 13:23:15 -0500

    edits to tagline

--- src/Components/Home/Tagline.js
+++ src/Components/Home/Tagline.js
@@ -4,7 +4,7 @@ class Tagline extends Component {
   render() {
     return (
         <p className="tagline">
-          Web developer able to build beautiful, easy to use websites with full database backend. Adept at working with diverse teams to implement projects. A quick study on new technologies.
+          Web developer able to build a beautiful, easy to use website with full database backend. Adept at working with diverse teams to implement projects. A quick study on new technologies, with the ability to proactively troubleshoot and solve issues.
         </p>
     )
   };
